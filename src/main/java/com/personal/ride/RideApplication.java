b9f4ec28d2d744345b0daea3e82aebe78d5ec8ba
sunpeikai <spk123456>
2020-05-11 09:30:26 +0800

    Initial commit

---
+++ src/main/java/com/personal/ride/RideApplication.java
@@ -0,0 +1,13 @@
+package com.personal.ride;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RideApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RideApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/personal/ride/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.personal.ride;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(RideApplication.class);
+    }
+
+}
